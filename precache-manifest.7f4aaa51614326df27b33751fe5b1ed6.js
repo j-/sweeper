d60708c7c629a64db290d0eac5b8b888dc905b35
j- <j@skeoh.com>
2020-06-27 14:51:43 +1000

    Updates

---
+++ precache-manifest.7f4aaa51614326df27b33751fe5b1ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f4016f71caa9ba28a88",
+    "url": "/sweeper/static/js/runtime~main.5f4016f7.js"
+  },
+  {
+    "revision": "e1a11b0a605ee9bf2741",
+    "url": "/sweeper/static/js/main.e1a11b0a.chunk.js"
+  },
+  {
+    "revision": "c2804b1974bbb7bfefd7",
+    "url": "/sweeper/static/js/1.c2804b19.chunk.js"
+  },
+  {
+    "revision": "e1a11b0a605ee9bf2741",
+    "url": "/sweeper/static/css/main.9211c096.chunk.css"
+  },
+  {
+    "revision": "c2804b1974bbb7bfefd7",
+    "url": "/sweeper/static/css/1.624fa37e.chunk.css"
+  },
+  {
+    "revision": "a93517811d98344aff2f88af0e4b77a3",
+    "url": "/sweeper/index.html"
+  }
+];
